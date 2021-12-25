829ec201976ddcc3af894626b486b1da2d219fa6
Michael L Perry <michael@qedcode.com>
2021-12-24 23:34:06 -0600

    Updated Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,136 +1,61 @@
 const path = require('path');
-const fs = require('fs');
-const nodeExternals = require('webpack-node-externals');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-function client() {
-  const views = fs.readdirSync(path.resolve(__dirname, 'views'))
-    .filter(file => path.extname(file) === '.html')
-    .map(file => path.basename(file, '.html'));
-  const scripts = fs.readdirSync(path.resolve(__dirname, 'src/client'))
-    .filter(file => path.extname(file) === '.jsx')
-    .map(file => path.basename(file, '.jsx'));
-  const names = views.filter(name => scripts.includes(name));
-
-  return {
-    // Input
-    entry: names.reduce((e, name) =>
-      ({ ...e, [name]: `./src/client/${name}.jsx` }),
-      {}
-    ),
-    resolve: {
-      extensions: ['.js', '.jsx', '.scss'],
-      alias: {
-        '@shared': path.resolve(__dirname, 'src/shared'),
-        'jinaga': 'jinaga/dist/jinaga',
-      },
-    },
-
-    // Processing
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          include: [
-            path.resolve(__dirname, 'src/client'),
-            path.resolve(__dirname, 'src/shared'),
-          ],
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-              },
-            },
-          ],
-          exclude: /node-modules/,
-        },
-        {
-            test: /\.(scss|css)$/,
-
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }
-      ]
-    },
-    plugins: names.map(name => new HtmlWebpackPlugin({
-      chunks: [name],
-      template: `views/${name}.html`,
-      filename: `${name}.html`
-    })).concat([
-      new WorkboxPlugin.GenerateSW({
-        swDest: 'scripts/service-worker.js',
-        inlineWorkboxRuntime: true
-      }),
-      new CopyWebpackPlugin([
-        {
-          from: path.resolve(__dirname, 'src/server/images'),
-          to:   path.resolve(__dirname, 'dist/images')
-        } 
-      ])
-    ]),
-
-    // Output
-    mode: 'production',
-    target: 'web',
-    devtool: 'source-map',
-    output: {
-      filename: 'scripts/[name]-[hash].js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-    },
-  };
-}
-
-const server = {
-  // Input
-  entry: './src/server/server.js',
+module.exports = {
+  // Inputs
+  entry: {
+    index: "./src/client/index.jsx",
+  },
   resolve: {
-    extensions: ['.js'],
+    extensions: [".js", ".jsx", ".scss"],
     alias: {
-      '@shared': path.resolve(__dirname, 'src/shared'),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "jinaga": "jinaga/dist/jinaga",
     },
   },
 
   // Processing
+  mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./views/index.html",
+      publicPath: "/scripts/",
+      filename: "../server/[name].html",
+    }),
+  ],
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src/server'),
-          path.resolve(__dirname, 'src/shared'),
-        ],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
+    rules: [{
+      test: /\.jsx?$/,
+      include: [
+        path.resolve(__dirname, "./src/client"),
+        path.resolve(__dirname, "./src/shared")
+      ],
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
-        ],
-        exclude: /node-modules/,
-      },
-    ],
+        },
+      ],
+      exclude: [/node_modules/]
+    },
+    {
+      test: /\.js$/,
+      enforce: "pre",
+      use: ["source-map-loader"],
+      include: [/node_modules/]
+    },
+    {
+      test: /\.(scss|css)$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    }]
   },
 
-  // Output
-  mode: 'production',
-  target: 'node',
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  devtool: 'source-map',
+  // Outputs
   output: {
-    filename: 'server.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name]-[contenthash].js",
+    path: path.resolve(__dirname, "dist", "scripts"),
   },
-  externals: [nodeExternals()],
+  devtool: "source-map",
 };
-
-module.exports = [
-  client(),
-  server
-];
